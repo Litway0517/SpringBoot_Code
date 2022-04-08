0d7a902aaf406c75dc2808fbb62326ed698247ed
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-08 20:57:59 +0800

    01_04_quickstart  @SpringBootApplication注解用来标志某个类为SpringBoot的启动类

---
+++ springboot_01_04_quickstart/src/main/java/com/ithiema/SpringBootApplicationTest.java
@@ -0,0 +1,14 @@
+package com.ithiema;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationTest.class);
+    }
+
+}
