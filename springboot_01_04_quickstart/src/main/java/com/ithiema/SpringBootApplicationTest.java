2dd77701f765d808c35a3f09e26c017cc9f33f9c
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-09 16:31:09 +0800

    01_04_quickstart  回滚提交

--- springboot_01_04_quickstart/src/main/java/com/ithiema/SpringBootApplicationTest.java
+++ springboot_01_04_quickstart/src/main/java/com/ithiema/SpringBootApplicationTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+// SpringBoot注解默认扫描的是com.itheima包下面的所有的相关的驱动注解, 例如@Component @Service等
 @SpringBootApplication
 public class SpringBootApplicationTest {
 
