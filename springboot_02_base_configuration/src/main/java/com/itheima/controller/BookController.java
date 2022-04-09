8a05d130866e4179c1893e6d4a0cde5f2d9793fc
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-09 20:48:11 +0800

    base_configuration  尝试读取配置数据 这里使用的是spring的注解@Value读取 温习

--- springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
+++ springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.itheima.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,20 @@ public class BookController {
     @GetMapping("/list")
     public String list() {
         System.out.println("获取图书列表...");
+        System.out.println("country ====> " + country);
+        System.out.println("username ====> " + username);
+        System.out.println("age ====> " + age);
         return "{'data':'获取图书列表...'}";
     }
 
+    // 读取YML文件中的内容
+    @Value("${country}")
+    private String country;
+
+    @Value("${users[1].name}")
+    private String username;
+
+    @Value("${users2[0].age}")
+    private String age;
 
 }
