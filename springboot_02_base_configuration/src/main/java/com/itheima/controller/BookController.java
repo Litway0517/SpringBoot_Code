0020baa58ee4df8deb63f9aeec220b1552c347a8
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-09 17:49:51 +0800

    base_configuration  properties配置是在应用的基础上的 就是pom.xml中的技术 如果没有使用某项技术那么就不会有配置提示

--- springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
+++ springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
@@ -2,15 +2,17 @@ package com.itheima.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/book")
+@RestController()
+@RequestMapping("/book")
 public class BookController {
 
     @GetMapping("/list")
     public String list() {
         System.out.println("获取图书列表...");
-        return "获取图书列表...";
+        return "{'data':'获取图书列表...'}";
     }
 
 
