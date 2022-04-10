8f4fc7a7379a2b075914f0aec78a8ae3d305e3da
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-10 11:44:24 +0800

    base_configuration MyDataSource对象的注入仍然需要使用@Autowired

--- springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
+++ springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
@@ -49,7 +49,11 @@ public class BookController {
     @Autowired
     private Environment environment;
 
-    // 使用一个实体类将YML中的数据加载进来 -> 常用方式
+    /*
+        使用一个实体类将YML中的数据加载进来 -> 常用方式
+        这个MyDataSource的注入权仍然在开发人员手中, 并不是一旦创建MyDataSource后就直接自动注入.
+            @ConfigurationProperties仅仅是标明 如果注入的话注入哪一段配置? 这个问题, 并不会进行注入这个动作
+     */
     @Autowired
     private MyDataSource dataSource;
 
