f424f4670f1044ba7dec2ae6d9c460278a41a95f
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-10 11:19:27 +0800

    base_configuration  搭配@Autowired注解使用Environment接口一次性将YML中的所有配置数据自动装配进来 使用个体getProperty("key")获取属性值

--- springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
+++ springboot_02_base_configuration/src/main/java/com/itheima/controller/BookController.java
@@ -1,12 +1,14 @@
 package com.itheima.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 @RequestMapping("/book")
 public class BookController {
 
@@ -18,6 +20,9 @@ public class BookController {
         System.out.println("age ====> " + age);
         System.out.println("dir ====> " + dir);
         System.out.println("dir2 ====> " + dir2);
+        System.out.println("--------------------------------");
+        System.out.println("所有配置数据environment ====> " + environment);
+        System.out.println("取出来某个environment中的数据 ====> " + environment.getProperty("users[0].name"));
         return "{'data':'获取图书列表...'}";
     }
 
@@ -36,5 +41,9 @@ public class BookController {
 
     @Value("${tempDir3}")
     private String dir2;
+
+    // 使用自动装配将所有的配置数据封装到一个对象中. 避免定义了上千个变量来接收配置中的数据
+    @Autowired
+    private Environment environment;
 
 }
