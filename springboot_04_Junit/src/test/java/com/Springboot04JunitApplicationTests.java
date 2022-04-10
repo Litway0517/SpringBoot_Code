86c10132a3e0e18d98972fbe2e6b596ca6186678
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-10 16:08:44 +0800

    Junit  如果测试类在引导类在引导类对应的包下或者子包下则不需要classes参数 否则需要对SpringBootTest(classes = "")加上classes参数指定引导类

--- springboot_04_Junit/src/test/java/com/Springboot04JunitApplicationTests.java
+++ springboot_04_Junit/src/test/java/com/Springboot04JunitApplicationTests.java
@@ -1,11 +1,17 @@
-package com.itheima;
+package com;
 
+import com.itheima.Springboot04JunitApplication;
 import com.itheima.dao.BookDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+// 当引导类和本例的测试类所在的包名不一样的时候, 需要用classes参数手动指定出来
+@SpringBootTest(classes = Springboot04JunitApplication.class)
+
+// @ContextConfiguration和@SpringBootTest一样的
+// @ContextConfiguration(classes = Springboot04JunitApplication.class)
 class Springboot04JunitApplicationTests {
 
     @Autowired
