d703dff8d8fd3133dd7e1af5a8c881f9bced135f
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-11 15:28:03 +0800

    SSMP  MyBatisPlus的分页拦截器一定要设置 @Configuration注解标注一个类为配置类并且自然地会注入到容器

---
+++ springboot_08_SSMP/src/main/java/com/litway/config/MyBatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.litway.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+// @Configuration将此类标注为配置类, 但是还会有一些基础功能, 就是将该类装入IOC容器. 本身就是Spring的注解, 自然而然会将对象装进去的
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // 添加分页插件
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+
+}
