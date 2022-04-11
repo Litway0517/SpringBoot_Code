77dbac9b20676e731acafc85fde17c3a6c4c7817
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-11 15:46:22 +0800

    SSMP  增加一下顺序 分页插件 增加条件查询

--- springboot_08_SSMP/src/test/java/com/litway/mapper/BookMapperTest01.java
+++ springboot_08_SSMP/src/test/java/com/litway/mapper/BookMapperTest01.java
@@ -1,5 +1,7 @@
 package com.litway.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.litway.SSMPApplication;
 import com.litway.entity.Book;
@@ -8,26 +10,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 
 @SpringBootTest(classes = SSMPApplication.class)
-class BookMapperTest {
+class BookMapperTest01 {
 
     @Autowired
     private BookMapper bookMapper;
 
     @Test
-    void testSelect() {
+    void testSelect01() {
         System.out.println(bookMapper.selectById(1L));
     }
 
     @Test
-    void testPage() {
+    void testPage02() {
         // 获取第二页数据, 每页数据3条
         Page<Book> page = new Page<Book>(2, 3);
         Page<Book> bookPage = bookMapper.selectPage(page, null);
         bookPage.getRecords().forEach(System.out::println);
     }
 
+    @Test
+    void testSelectByCondition03() {
+        String column = "spring";
+        LambdaQueryWrapper<Book> lambdaQuery = new LambdaQueryWrapper<Book>();
+        lambdaQuery.like(StringUtils.isNotBlank(column), Book::getName, column);
+        List<Book> books = bookMapper.selectList(lambdaQuery);
+        System.out.println(books);
+    }
 }
+
