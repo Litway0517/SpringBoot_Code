7b949f26762a36267bdd971a6eeb3ace3af50557
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-11 11:38:43 +0800

    SSMP  ORM映射 Mapper层 MyBatisPlus框架的配置信息

---
+++ springboot_08_SSMP/src/main/java/com/litway/mapper/BookMapper.java
@@ -0,0 +1,12 @@
+package com.litway.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.litway.entity.Book;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BookMapper extends BaseMapper<Book> {
+
+
+
+}
