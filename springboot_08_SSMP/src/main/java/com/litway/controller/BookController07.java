f2530a71fcc9385e5cde0d2cb39a1aca395dddd4
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-12 11:03:39 +0800

    SSMP  controller层增删查改和分页操作

---
+++ springboot_08_SSMP/src/main/java/com/litway/controller/BookController07.java
@@ -0,0 +1,54 @@
+package com.litway.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.litway.entity.Book;
+import com.litway.service.IBookService05;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController07 {
+
+    @Autowired
+    private IBookService05 bookService;
+
+    @GetMapping("/list")
+    public List<Book> list() {
+        return bookService.list();
+    }
+
+    @PostMapping
+    public boolean save(@RequestBody Book book) {
+        // 通过异步请求的请求体JSON串提交过来
+        return bookService.save(book);
+    }
+
+    // 和视频不一样
+    @PutMapping
+    public boolean update(@RequestBody Book book) {
+        return bookService.updateById(book);
+    }
+
+    // 和视频不一样
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") Integer id) {
+        return bookService.removeById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable("id") Integer id) {
+        return bookService.getById(id);
+    }
+
+    @GetMapping("/{currentPage}/{pageSize}")
+    public Page<Book> page(@PathVariable("currentPage") int currentPage, @PathVariable("pageSize") int pageSize) {
+        return bookService.getPage(currentPage, pageSize);
+    }
+
+
+
+}
