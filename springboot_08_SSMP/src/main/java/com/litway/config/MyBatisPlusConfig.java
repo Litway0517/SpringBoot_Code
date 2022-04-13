d2a84e8f697f551e4bb5995d09532998d96ab055
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-13 21:04:02 +0800

    SSMP  MyBatisPlus的乐观锁插件是为了解决并发问题的 插件名称OptimisticLocker

--- springboot_08_SSMP/src/main/java/com/litway/config/MyBatisPlusConfig.java
+++ springboot_08_SSMP/src/main/java/com/litway/config/MyBatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.litway.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,8 @@ public class MyBatisPlusConfig {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         // 添加分页插件
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁插件
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return mybatisPlusInterceptor;
     }
 
