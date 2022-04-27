03247caf56ce25c01253e8fbd7956f4fd13da76d
Litway_ <xmw0517@gmail.com>
2022-04-27 21:41:19 +0800

    SSMP  SpringBoot在启动的时候args参数就是菜单卡里面填写的参数 当然也能够创建一个字符串数组传入SpringBoot的run方法

--- springboot_08_SSMP/src/main/java/com/litway/SSMPApplication.java
+++ springboot_08_SSMP/src/main/java/com/litway/SSMPApplication.java
@@ -3,6 +3,8 @@ package com.litway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 /**
  * ssmpapplication
  *
@@ -17,7 +19,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SSMPApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SSMPApplication.class, args);
+        // 这里的args就是启动时传入的参数, 这个参数和上面工具栏锤子右面的编辑菜单有联系. 就是填写的参数
+        System.out.println(Arrays.toString(args));
+
+        // 当然也能够手动传参
+        String[] arg = new String[1];
+        arg[0] = "--server.port=8082";
+        SpringApplication.run(SSMPApplication.class, arg);
     }
 
 }
