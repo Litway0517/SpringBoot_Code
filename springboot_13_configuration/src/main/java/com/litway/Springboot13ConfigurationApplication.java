cda0c5970a4d58ac49402d9b013f8c45fabb50f0
Litway_ <xmw0517@gmail.com>
2022-05-03 21:40:11 +0800

    高级配置configuration  @ConfigurationProperties(prefix = "配置的上一级名称")注解也能够用于第三方Bean的配置注入 第三方就是指的他人开发的例如druid

--- springboot_13_configuration/src/main/java/com/litway/Springboot13ConfigurationApplication.java
+++ springboot_13_configuration/src/main/java/com/litway/Springboot13ConfigurationApplication.java
@@ -1,17 +1,32 @@
 package com.litway;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.litway.config.ServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Springboot13ConfigurationApplication {
+
+	@Bean
+	@ConfigurationProperties(prefix = "datasource")
+	public DruidDataSource dataSource() {
+		// 德鲁伊的数据加载是懒加载
+		DruidDataSource ds = new DruidDataSource();
+		// ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		return ds;
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Springboot13ConfigurationApplication.class, args);
 		ServerConfig bean = run.getBean(ServerConfig.class);
 		System.out.println(bean);
+		DruidDataSource ds = run.getBean(DruidDataSource.class);
+		System.out.println(ds);
+		System.out.println(ds.getDriverClassName());
 
 	}
 
