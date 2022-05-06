f7102519311a619817fce4b6e940a32ef4b25fda
Litway_ <xmw0517@gmail.com>
2022-05-06 17:57:20 +0800

    高级配置configuration  在Bean上加上@Validated注解标志该类需要被检查 对字段使用具体的@Max @Min @Length @Range等众多注解标志详细的校验规则

--- springboot_13_configuration/src/main/java/com/litway/config/ServerConfig.java
+++ springboot_13_configuration/src/main/java/com/litway/config/ServerConfig.java
@@ -7,7 +7,9 @@ import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -17,6 +19,9 @@ import java.time.temporal.ChronoUnit;
 
 // 加载该类时同时将配置文件中的配置赋值到该类中, 前缀是具体配置的上一层, 多层的话就按照下面这样写
 @ConfigurationProperties(prefix = "servers.a.b.c")
+
+// 加上@Validated注解标识此类需要做校验
+@Validated
 public class ServerConfig {
 
     private String ipAddress;
@@ -31,5 +36,9 @@ public class ServerConfig {
     // 单位分为字节 KB MB GB TB. 默认为字节
     @DataSizeUnit(DataUnit.KILOBYTES)
     private DataSize dataSize;
+
+    // 校验最大值不能超过8888, 否则提示message消息
+    @Max(value = 8888, message = "最大值不能超过8888")
+    private int testData;
 
 }
