66217312db91b827fe6a654955d1b3a799971984
Litway_ <xmw0517@gmail.com>
2022-05-09 21:12:24 +0800

    test  @SpringBootTest(webEnvironment=..DEFINE_PORT)测表现层 加上这个参数能够启动tomcat服务器

---
+++ springboot_14_test/src/test/java/com/litway/WebTest.java
@@ -0,0 +1,14 @@
+package com.litway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class WebTest {
+
+    @Test
+    void test() {
+
+    }
+
+}
