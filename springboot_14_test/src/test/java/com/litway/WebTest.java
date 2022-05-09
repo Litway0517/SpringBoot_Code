18c9ade7aac62bf7796c4e1d8e27af938fbc1da3
Litway_ <xmw0517@gmail.com>
2022-05-09 21:44:14 +0800

    test  webEnvironment的可选值

--- springboot_14_test/src/test/java/com/litway/WebTest.java
+++ springboot_14_test/src/test/java/com/litway/WebTest.java
@@ -3,6 +3,7 @@ package com.litway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// webEnvironment参数的可选值为: RANDOM_PORT(随机生成端口) | DEFINE_PORT(使用配置文件的) | MOCK | NONE(无端口)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class WebTest {
 
