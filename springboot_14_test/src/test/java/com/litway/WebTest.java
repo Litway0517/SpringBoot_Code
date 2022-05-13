06d63ed2b91d26c8654f70a91fb7de8752b74073
Litway_ <xmw0517@gmail.com>
2022-05-13 23:01:12 +0800

    test  @AutoConfigurationMockMvc注解是开启虚拟的MVC调用 此时就能够注入MockMvc对象使用了

--- springboot_14_test/src/test/java/com/litway/WebTest.java
+++ springboot_14_test/src/test/java/com/litway/WebTest.java
@@ -1,16 +1,32 @@
 package com.litway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 // webEnvironment参数的可选值为: RANDOM_PORT(随机生成端口) | DEFINE_PORT(使用配置文件的) | MOCK | NONE(无端口)
 // SpringBootTest的参数webEnvironment作用是用来启动web环境
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+
+// AutoConfigureMockMvc开启虚拟的MVC调用
+@AutoConfigureMockMvc
 public class WebTest {
+
+    // 自动装配的方式注入mockMvc对象
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     void test() {
 
     }
+
+    @Test
+    void testWeb() {
+    }
+
+
 
 }
