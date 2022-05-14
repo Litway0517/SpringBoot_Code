1ce3eb192e86ea28c8f9a31ad8fdf2ed5a363818
Litway_ <xmw0517@gmail.com>
2022-05-14 22:12:26 +0800

    test  MockMvc的perform方法能够执行虚拟请求 但是虚拟请求需要通过MockMvcRequestBuilders对象来设定

--- springboot_14_test/src/test/java/com/litway/WebTest.java
+++ springboot_14_test/src/test/java/com/litway/WebTest.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 // webEnvironment参数的可选值为: RANDOM_PORT(随机生成端口) | DEFINE_PORT(使用配置文件的) | MOCK | NONE(无端口)
 // SpringBootTest的参数webEnvironment作用是用来启动web环境
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 // AutoConfigureMockMvc开启虚拟的MVC调用
 @AutoConfigureMockMvc
@@ -24,7 +26,16 @@ public class WebTest {
     }
 
     @Test
-    void testWeb() {
+    void testWeb1() {
+        System.out.println(mockMvc);
+    }
+
+    @Test
+    void testWeb2(@Autowired MockMvc mvc) throws Exception {
+        // 创建一个虚拟请求 -> http:ip:port/books
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        // 执行请求
+        mvc.perform(builder);
     }
 
 
