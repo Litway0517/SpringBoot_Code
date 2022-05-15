1b9589fe965b366b58f0a4a254637f536ada8837
Litway_ <xmw0517@gmail.com>
2022-05-15 21:02:39 +0800

    test  MockMvcRequestBuilders对象用来发送请求 MockMvc对象(需要使用@AutoConfigureMockMvc注解注入)发送请求(返回ResultActions对象) 使用MockMvcResultMatchers对象用来设定请求的预计值  ResultActions和ResultMatcher进行比较

--- springboot_14_test/src/test/java/com/litway/WebTest.java
+++ springboot_14_test/src/test/java/com/litway/WebTest.java
@@ -5,8 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.RequestResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
 // webEnvironment参数的可选值为: RANDOM_PORT(随机生成端口) | DEFINE_PORT(使用配置文件的) | MOCK | NONE(无端口)
 // SpringBootTest的参数webEnvironment作用是用来启动web环境
@@ -32,10 +37,25 @@ public class WebTest {
 
     @Test
     void testWeb2(@Autowired MockMvc mvc) throws Exception {
+        /*
+            S1- 模拟请求
+            S2- 发送请求
+            S3- 接收请求并验证
+         */
         // 创建一个虚拟请求 -> http:ip:port/books
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
-        // 执行请求
-        mvc.perform(builder);
+        // 发送请求, 会有一个返回值
+        ResultActions perform = mvc.perform(builder);
+
+        // 设定预期值 与 真实值进行比较, 成功则测试通过, 否则测试失败
+        // 定义本次调用的预期值, 是一个叫做ResultMatcher的对象
+        StatusResultMatchers status = MockMvcResultMatchers.status();
+        // 预计本次调用是成功的, status为200
+        ResultMatcher ok = status.isOk();
+
+        // 添加预计值到本次调用中, 进行匹配
+        perform.andExpect(ok);
+
     }
 
 
