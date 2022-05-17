f4e1e2532969d1d0d1082f7e88eafcd2a0b9cbdd
Litway_ <xmw0517@gmail.com>
2022-05-17 22:24:51 +0800

    test  点亮

--- springboot_14_test/src/test/java/com/litway/WebTest.java
+++ springboot_14_test/src/test/java/com/litway/WebTest.java
@@ -13,6 +13,10 @@ import org.springframework.test.web.servlet.result.ContentResultMatchers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
+/**
+ * 表现层测试
+ */
+
 // webEnvironment参数的可选值为: RANDOM_PORT(随机生成端口) | DEFINE_PORT(使用配置文件的) | MOCK | NONE(无端口)
 // SpringBootTest的参数webEnvironment作用是用来启动web环境
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
