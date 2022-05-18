28569a2549db3e963c960d709eab1e4c3091172b
Litway_ <xmw0517@gmail.com>
2022-05-18 22:35:54 +0800

    test

--- springboot_14_test/src/test/java/com/litway/PropertiesAndArgsTest.java
+++ springboot_14_test/src/test/java/com/litway/PropertiesAndArgsTest.java
@@ -9,6 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 // SpringBoot的properties参数能够 为当前测试环境添加临时配置属性, 而不是使用开发环境的配置文件作为配置
 // @SpringBootTest(properties = {"test.prop=testValue1"})
 
+/**
+ * 属性和参数测试
+ */
+
 // SpringBoot的args参数也能够 为当前测试环境添加临时属性, 只不过有一点不一样
 @SpringBootTest(args = {"--test.prop=8081"})
 
