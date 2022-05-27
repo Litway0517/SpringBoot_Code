e9fdde41d28e35ba45983a9a60ba18e6470529d0
Litway_ <xmw0517@gmail.com>
2022-05-27 23:04:08 +0800

    test  记录

--- springboot_14_test/src/main/java/com/litway/controller/BookController.java
+++ springboot_14_test/src/main/java/com/litway/controller/BookController.java
@@ -2,7 +2,6 @@ package com.litway.controller;
 
 
 import com.litway.entity.Book;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
