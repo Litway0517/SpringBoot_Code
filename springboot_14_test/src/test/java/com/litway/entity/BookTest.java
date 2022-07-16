e142c17d2a207c9afd3f355dc43bc8ca99d191f7
Litway_ <xmw0517@gmail.com>
2022-07-16 21:04:39 +0800

    SSMP  字符串转拼音 如果是汉字就转换 如果是字母或者数字就保留

--- springboot_14_test/src/test/java/com/litway/entity/BookTest.java
+++ springboot_14_test/src/test/java/com/litway/entity/BookTest.java
@@ -7,6 +7,9 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
@@ -17,7 +20,37 @@ class BookTest {
         System.out.println(s.contains("254345345"));
     }
 
-    
+    /**
+     * 助记符用到的函数
+     * @param chinese 中文字符串
+     * @return 中文字符串 -> ZWZFC
+     */
+    public static String ToFirstChar(String chinese){
+        String pinyinStr = "";
+        char[] newChar = chinese.toCharArray();  //转为单个字符
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        for (int i = 0; i < newChar.length; i++) {
+            if (newChar[i] > 128) {
+                try {
+                    pinyinStr += PinyinHelper.toHanyuPinyinStringArray(newChar[i], defaultFormat)[0].charAt(0);
+                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                    e.printStackTrace();
+                }
+            }else{
+                pinyinStr += newChar[i];
+            }
+        }
+        return pinyinStr;
+    }
+
+    @Test
+    public void testPinyin() {
+        System.out.println(ToFirstChar("0125asd测试A12"));
+    }
+
+
 
 
 
