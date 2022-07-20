ee90a4413b302c04199366a5c42476c6c49caf68
Litway_ <xmw0517@gmail.com>
2022-07-20 23:58:00 +0800

    SSMP

---
+++ springboot_14_test/src/test/java/com/litway/Test.java
@@ -0,0 +1,15 @@
+package com.litway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        String s = "图文编号 " + "254345345" + " 已经存在, 请重新操作. ";
+        System.out.println(s.contains("254345345"));
+    }
+
+
+}
